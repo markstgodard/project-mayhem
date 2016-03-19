872adaa8304addc0e029d1edce4e133d226c60ac
Mark St.Godard <markstgodard@gmail.com>
2016-03-19 16:00:31 -0500

    cleanup port

--- main.go
+++ main.go
@@ -13,17 +13,19 @@ const (
 )
 
 func main() {
-	var port string
-	if port = os.Getenv("PORT"); len(port) == 0 {
-		port = DEFAULT_PORT
-	}
-
 	http.HandleFunc("/", index)
-
 	log.Printf("Listening on %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+getPort(), nil))
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "nothing to see yet")
+}
+
+func getPort() string {
+	var port string
+	if port = os.Getenv("PORT"); len(port) == 0 {
+		port = DEFAULT_PORT
+	}
+	return port
 }
